5a1a30d08ab01ae1dd76f104d852aab69547e437
Mohamed Fathi <Mohamedfathi11119@gmail.com>
2023-02-22 21:08:56 +0200

    inerface_class_added
    add the important classes to the project

---
+++ SW_assignment_1/src/Board.java
@@ -0,0 +1,17 @@
+public abstract class Board {
+
+    protected int  n;
+    protected char grid[][];
+
+    // Set n and book space for n x n grid
+    Board (int n){};
+    // Return true  if move is valid and put it on board or return false
+    abstract boolean update_board (int x, int y, char symbol) ;
+    // Returns true if symbol owner wins
+    abstract boolean is_winner(char symbol) ;
+    // Return true if board status indicates a draw
+    abstract boolean is_draw() ;
+    // Display the board and the pieces on it
+    abstract void display_board();
+};
+
